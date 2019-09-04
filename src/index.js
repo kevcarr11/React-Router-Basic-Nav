d1484285d568f69d9c3f687b9e0d61e66859a3da
Your Name <kevincarr@Kevins-MacBook-Pro.local>
2019-09-04 18:13:52 -0500

    declared all routes

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Home } from "./components/Home";
+
+
+ReactDOM.render(
+<Router>
+ <App />
+</Router>, document.getElementById('root')
+);

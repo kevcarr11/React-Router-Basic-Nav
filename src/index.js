607acc65dbe22b5ffe6a065ec001daf364323bd5
Your Name <kevincarr@Kevins-MacBook-Pro.local>
2019-09-04 19:01:37 -0500

    part 1 completed

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter as Router } from "react-router-dom";
 
-import { Home } from "./components/Home";
+
 
 
 ReactDOM.render(
